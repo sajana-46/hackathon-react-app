8bccb6a92b1b42a9fc5b76226498052278a60295
sajana-46 <sajanaskuttiyil@gmail.com>
2026-07-14 21:17:22 +0530

    navigation bar completed

--- hackathon-app/src/App.jsx
+++ hackathon-app/src/App.jsx
@@ -5,14 +5,27 @@ import heroImg from './assets/hero.png'
 import './App.css'
 import Addteam from './components/Addteam'
 import Viewteam from './components/Viewteam'
+import Navigationbar from './components/Navigationbar'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Addteam/>
-    <Viewteam/>
+     <BrowserRouter>
+    <Routes>
+            
+
+
+      <Route path='/' element={<Addteam/>} />
+      <Route path='/view' element={<Viewteam/>} />
+
+    </Routes>
+    </BrowserRouter>
+      
+    
       
     </>
   )
